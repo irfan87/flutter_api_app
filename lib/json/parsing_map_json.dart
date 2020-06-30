5bbd1073e416f5cbec4cbfb53cf1517d44047f3d
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-07-01 00:05:40 +0800

    finalized PODO

--- lib/json/parsing_map_json.dart
+++ lib/json/parsing_map_json.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_api_app/utils/json_mapping_network_post.dart';
+import 'package:flutter_api_app/utils/json_network.dart';
 
 class ParsingMapJSON extends StatefulWidget {
   @override
@@ -7,8 +8,73 @@ class ParsingMapJSON extends StatefulWidget {
 }
 
 class _ParsingMapJSONState extends State<ParsingMapJSON> {
+  Future<JSONMappingNetworkPostList> data;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    JSONNetwork network =
+        JSONNetwork('https://jsonplaceholder.typicode.com/posts');
+    data = network.loadPost();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PODO'),
+      ),
+      body: Center(
+        child: Container(
+          child: FutureBuilder(
+            future: data,
+            builder:
+                (context, AsyncSnapshot<JSONMappingNetworkPostList> snapshot) {
+              List<JSONMappingNetworkPost> allPosts;
+
+              if (snapshot.hasData) {
+                allPosts = snapshot.data.posts;
+
+                return createListView(allPosts, context);
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget createListView(
+      List<JSONMappingNetworkPost> data, BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, int index) {
+          return Column(
+            children: <Widget>[
+              Divider(
+                height: 5.0,
+              ),
+              ListTile(
+                title: Text(data.elementAt(index).title),
+                subtitle: Text(data.elementAt(index).body),
+                leading: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      radius: 23.0,
+                      child: Text("${data.elementAt(index).userId}"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+// change the home at MaterialApp with ParsingMapJSON for temporary
 import 'package:flutter_api_app/json/parsing_json.dart';
+import 'package:flutter_api_app/json/parsing_map_json.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter API App',
-      home: ParsingJSON(),
+      home: ParsingMapJSON(),
     );
   }
 }
